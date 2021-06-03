10c03505cc790143ecab65ae772828714b2434f2
holgstr <hbj.loewe@gmail.com>
2021-06-03 18:01:23 +0200

    fasttext almost done for Task A

--- Playground.py
+++ Playground.py
@@ -2,8 +2,8 @@
 import csv
 import numpy as np
 import pandas as pd
+import fasttext
 from gensim.utils import simple_preprocess
-from gensim.models import FastText
 
 # Daten einlesen
 header_list = ["URL", "Document", "Relevance", "Polarity"]
@@ -29,6 +29,11 @@ dat_train[['Document', 'Relevance']].to_csv('ft_train_A.txt', index = False, sep
 dat_dev[['Document', 'Relevance']].to_csv('ft_dev_A.txt', index = False, sep = ' ', header = None, quoting = csv.QUOTE_NONE, quotechar = "", escapechar = " ")
 
 # Train fastText Task A
-## HIER EVT MIT GENSIM?
-##fasttext findet es bei site-packages nur unter gensim, nicht selbst
-model_ft_A = FastText.train_supervised('ft_train_A.txt', wordNgrams = 2)
+model_ft_A = fasttext.train_supervised('ft_train_A.txt', wordNgrams = 2)
+
+# Test fastText Task A
+evaluation_model_ft_A = model_ft_A.test('ft_dev_A.txt')[1:3]  # precision and recall
+model_ft_A.predict(dat_dev.iloc[86,1]) # predict individual observation
+
+# Performance mit Micro-Averaged F1-Score (Berechnung hier funktioniert nur bei 2 Klassen)
+micro_f1_ft_A = 2*(evaluation_model_ft_A[0]*evaluation_model_ft_A[1]/(evaluation_model_ft_A[0]+evaluation_model_ft_A[1]))
