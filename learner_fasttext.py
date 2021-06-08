82c7b716f87912e93ffe3076c82b486083b1b06b
holgstr <hbj.loewe@gmail.com>
2021-06-08 04:41:17 +0200

    next step will be optimizing hyperparameter tuning, potentially even manually

--- learner_fasttext.py
+++ learner_fasttext.py
@@ -28,39 +28,32 @@ def fasttext_df_preprocess(df, df_name):
   df[['Document', 'Polarity']].to_csv(os.path.join('ft_' + df_name + '_B.txt'), index=False, sep=' ', header=None,
                                              quoting=csv.QUOTE_NONE, quotechar="", escapechar=" ")
   return df
-
 dat_train = fasttext_df_preprocess(dat_train, 'train')
 dat_dev = fasttext_df_preprocess(dat_dev, 'dev')
 dat_test = fasttext_df_preprocess(dat_test, 'test')
 
 # Train fastText
-#model_ft_A = fasttext.train_supervised('ft_train_A.txt', wordNgrams = 2, epoch = 30)
-#model_ft_B = fasttext.train_supervised('ft_train_B.txt', wordNgrams = 2, epoch = 30)
-model_ft_A = fasttext.train_supervised(input='ft_train_A.txt', autotuneValidationFile='ft_dev_A.txt', autotuneDuration=240)
-model_ft_B = fasttext.train_supervised(input='ft_train_B.txt', autotuneValidationFile='ft_dev_B.txt', autotuneDuration=240)
+model_ft_A = fasttext.train_supervised('ft_train_A.txt', wordNgrams = 2, epoch = 30)
+model_ft_B = fasttext.train_supervised('ft_train_B.txt', wordNgrams = 2, epoch = 30)
+model_ft_A = fasttext.train_supervised(input='ft_train_A.txt', autotuneValidationFile='ft_dev_A.txt', autotuneDuration=300)
+model_ft_B = fasttext.train_supervised(input='ft_train_B.txt', autotuneValidationFile='ft_dev_B.txt', autotuneDuration=300)
 
 # Test fastText
-evaluation_model_ft_A_dev = model_ft_A.test('ft_dev_A.txt')[1:3]  # precision and recall
-evaluation_model_ft_B_dev = model_ft_B.test('ft_dev_B.txt')[1:3]  # precision and recall
-evaluation_model_ft_A_test = model_ft_A.test('ft_test_A.txt')[1:3]  # precision and recall
-evaluation_model_ft_B_test = model_ft_B.test('ft_test_B.txt')[1:3]  # precision and recall
-model_ft_A.predict(dat_dev.iloc[86,1])  # predict individual observation
-model_ft_B.predict(dat_dev.iloc[86,1])  # predict individual observation
-model_ft_A.predict(dat_test.iloc[86,1])  # predict individual observation
-model_ft_B.predict(dat_test.iloc[86,1])  # predict individual observation
+def m_f1(model, eval_file):
+  p = model.test(eval_file)[1]
+  r = model.test(eval_file)[2]
+  return 2*(p*r/(p+r))
 
-# Performance mit Micro-Averaged F1-Score (Berechnung hier funktioniert nur bei 2 Klassen)
-micro_f1_ft_A_dev = 2*(evaluation_model_ft_A_dev[0]*evaluation_model_ft_A_dev[1]/(evaluation_model_ft_A_dev[0]+evaluation_model_ft_A_dev[1]))
-micro_f1_ft_B_dev = 2*(evaluation_model_ft_B_dev[0]*evaluation_model_ft_B_dev[1]/(evaluation_model_ft_B_dev[0]+evaluation_model_ft_B_dev[1]))
-micro_f1_ft_A_test = 2*(evaluation_model_ft_A_test[0]*evaluation_model_ft_A_test[1]/(evaluation_model_ft_A_test[0]+evaluation_model_ft_A_test[1]))
-micro_f1_ft_B_test = 2*(evaluation_model_ft_B_test[0]*evaluation_model_ft_B_test[1]/(evaluation_model_ft_B_test[0]+evaluation_model_ft_B_test[1]))
+f1_ft = (pd.Series([m_f1(model_ft_A, 'ft_dev_A.txt'), m_f1(model_ft_A, 'ft_test_A.txt'),
+                    m_f1(model_ft_B, 'ft_dev_B.txt'),m_f1(model_ft_B, 'ft_test_B.txt')],
+                   index = ["ft_A_dev", "ft_A_test", "ft_dev_B.txt", "ft_test_B.txt"]))
 
 # Confusion Matrix
-dat_dev['Relevance_predicted_ft'] = dat_dev['Document'].apply(lambda x: model_ft_A.predict(x)[0][0])
-dat_dev['Polarity_predicted_ft'] = dat_dev['Document'].apply(lambda x: model_ft_B.predict(x)[0][0])
-dat_test['Relevance_predicted_ft'] = dat_test['Document'].apply(lambda x: model_ft_A.predict(x)[0][0])
-dat_test['Polarity_predicted_ft'] = dat_test['Document'].apply(lambda x: model_ft_B.predict(x)[0][0])
-confusion_matrix(dat_dev['Relevance'], dat_dev['Relevance_predicted_ft'], normalize= 'all')
-confusion_matrix(dat_test['Relevance'], dat_test['Relevance_predicted_ft'], normalize= 'all')
-confusion_matrix(dat_dev['Polarity'], dat_dev['Polarity_predicted_ft'], normalize= 'all')
-confusion_matrix(dat_test['Polarity'], dat_test['Polarity_predicted_ft'], normalize= 'all')
+  dat_dev['Relevance_predicted_ft'] = dat_dev['Document'].apply(lambda x: model_ft_A.predict(x)[0][0])
+  dat_dev['Polarity_predicted_ft'] = dat_dev['Document'].apply(lambda x: model_ft_B.predict(x)[0][0])
+  dat_test['Relevance_predicted_ft'] = dat_test['Document'].apply(lambda x: model_ft_A.predict(x)[0][0])
+  dat_test['Polarity_predicted_ft'] = dat_test['Document'].apply(lambda x: model_ft_B.predict(x)[0][0])
+  confusion_matrix(dat_dev['Relevance'], dat_dev['Relevance_predicted_ft'], normalize= 'all')
+  confusion_matrix(dat_test['Relevance'], dat_test['Relevance_predicted_ft'], normalize= 'all')
+  confusion_matrix(dat_dev['Polarity'], dat_dev['Polarity_predicted_ft'], normalize= 'all')
+  confusion_matrix(dat_test['Polarity'], dat_test['Polarity_predicted_ft'], normalize= 'all')
