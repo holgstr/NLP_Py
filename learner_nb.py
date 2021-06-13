3f83c0884b0c803e835777bd638d8eb1946478e6
holgstr <hbj.loewe@gmail.com>
2021-06-13 02:57:58 +0200

    NB naive classifiers completed, performance weak, tuning not yet initiated

--- learner_nb.py
+++ learner_nb.py
@@ -1,6 +1,11 @@
 # Pakete importieren
+import csv
+import numpy as np
+import pandas as pd
+from gensim.utils import simple_preprocess
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import f1_score
 
 # Daten einlesen
 header_list = ["URL", "Document", "Relevance", "Polarity"]
@@ -9,7 +14,7 @@ dat_dev = pd.read_csv('/Users/holgerlowe/Documents/NLP_Data/dev.tsv', names = he
 dat_syn = pd.read_csv('/Users/holgerlowe/Documents/NLP_Data/syn.tsv', names = header_list, usecols = range(0,4), sep = '\t')
 dat_dia = pd.read_csv('/Users/holgerlowe/Documents/NLP_Data/dia.tsv', names = header_list, usecols = range(0,4), sep = '\t')
 
-# Daten vorbereiten für fasttext
+# Daten vorbereiten für Naive Bayes
 def nb_df_preprocess(df):
   # Delete NAs
   df = df.dropna(subset=['URL', 'Document']).reset_index(drop=True)
@@ -25,7 +30,27 @@ dat_dev = nb_df_preprocess(dat_dev)
 dat_syn = nb_df_preprocess(dat_syn)
 dat_dia = nb_df_preprocess(dat_dia)
 
+# Compute Micro-F1 for given model and eval_file
+def m_f1(model, eval_file, task):
+  if task == 'A':
+    problem = 'Relevance'
+  else:
+    problem = 'Polarity'
+  return f1_score(eval_file[problem], model.predict(vectorizer.transform(eval_file['Document'])), average='micro')
+
+# Train Naive Bayes Classifier
 vectorizer = CountVectorizer()
-x_train_vectorized = vectorizer.fit_transform(dat_train['Document'])
-y_train_nb = np.array(dat_train['Relevance'])
 model_nb_A = BernoulliNB(alpha=1.0)
+model_nb_A.fit(vectorizer.fit_transform(dat_train['Document']), np.array(dat_train['Relevance']))
+model_nb_B = BernoulliNB(alpha=1.0)
+model_nb_B.fit(vectorizer.fit_transform(dat_train['Document']), np.array(dat_train['Polarity']))
+
+# Test Naive Bayes
+f1_nb = pd.DataFrame(data={'data': ["dev_A", "syn_A", "dia_A", "dev_B", "syn_B", "dia_B"],
+                           'naive': [m_f1(model_nb_A, dat_dev, 'A'), m_f1(model_nb_A, dat_syn, 'A'),
+                                     m_f1(model_nb_A, dat_dia, 'A'), m_f1(model_nb_B, dat_dev, 'B'),
+                                     m_f1(model_nb_B, dat_syn, 'B'), m_f1(model_nb_B, dat_dia, 'B')],
+                           'tuned': [m_f1(model_nb_A, dat_dev, 'A'), m_f1(model_nb_A, dat_syn, 'A'),
+                                     m_f1(model_nb_A, dat_dia, 'A'), m_f1(model_nb_B, dat_dev, 'B'),
+                                     m_f1(model_nb_B, dat_syn, 'B'), m_f1(model_nb_B, dat_dia, 'B')]})
+f1_nb
